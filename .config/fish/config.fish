5f9e8b6ce70024824cff551827e70324be0ae374
marcos <emipeceemi@gmail.com>
2025-04-16 09:25:28 -0300

    Patched Fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,23 @@
+#!/usr/bin/fish
+
+set fish_greeting
+
+function fish_prompt
+    set hex cba6f7 # Catppuccin Mocha Mauve
+    set prompt (basename (prompt_pwd))
+    echo " $(set_color -o $hex) $prompt $(set_color normal) "
+end
+
+function md
+    set cmd $argv[1]
+    set args $argv[2..-2]
+    set dir $argv[-1]
+
+    # Avoid overlapping
+    set args (string replace -a "^" ">" $args)
+    set args (string replace -a "!" "|" $args)
+    set path (string match -r ".*\/" $dir)
+
+    mkdir -p $path
+    eval $cmd $args $dir
+end
